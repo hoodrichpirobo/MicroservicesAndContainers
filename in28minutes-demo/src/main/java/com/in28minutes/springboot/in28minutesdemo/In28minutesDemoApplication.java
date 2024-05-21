1c3b739990f13298062a5a08d92d8afd66693d5a
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2024-05-21 19:11:44 +0200

    wasted my afternoon tryna use git on eclipse

---
+++ in28minutes-demo/src/main/java/com/in28minutes/springboot/in28minutesdemo/In28minutesDemoApplication.java
@@ -0,0 +1,14 @@
+package com.in28minutes.springboot.in28minutesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class In28minutesDemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World! Pt 1");
+		SpringApplication.run(In28minutesDemoApplication.class, args);
+	}
+
+}
