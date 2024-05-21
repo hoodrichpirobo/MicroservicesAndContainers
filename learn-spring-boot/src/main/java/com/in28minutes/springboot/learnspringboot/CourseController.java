818fb1e0589866d45755080717a76d756ee7565b
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2024-05-21 19:41:18 +0200

    simple rest api on top of springboot app

---
+++ learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
@@ -0,0 +1,16 @@
+package com.in28minutes.springboot.learnspringboot;
+
+import java.util.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CourseController {
+
+	@RequestMapping("/courses")
+	public List<Course> retrieveallCourses(){
+		return Arrays.asList(
+				new Course(1, "Learn AWS", "in28minutes"),
+				new Course(2, "Learn DevOps", "in28minutes")
+				);
+	}
+}
