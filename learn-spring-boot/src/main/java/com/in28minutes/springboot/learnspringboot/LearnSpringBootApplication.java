fe0f789fabb5b18b5ed6473a3e9d4c729fc5dd15
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2024-05-21 20:17:21 +0200

    Update LearnSpringBootApplication.java

--- learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/LearnSpringBootApplication.java
+++ learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/LearnSpringBootApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearnSpringBootApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world! Pt2");
 		SpringApplication.run(LearnSpringBootApplication.class, args);
 	}
 
